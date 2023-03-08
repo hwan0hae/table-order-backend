68c13e8ab489b766afa060d5e75fa9743dfb3fa5
hwan0hae <eoghkszn@gmail.com>
2023-03-08 19:44:01 +0900

    feat: App order post api 추가, webSocket 추가, 소켓 통신 주문데이터 전달

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,39 @@
 import express from "express";
+import http from "http";
+import socketIO, { Socket } from "socket.io";
 import cors from "cors";
 import { getMenu } from "./controller/menu";
+import { order } from "./controller/order";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new socketIO.Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+export const socketArr = new Array<Socket>();
+
+io.on("connection", (socket) => {
+  socketArr.push(socket);
+  console.log("New client connected");
+
+  socket.on("disconnect", () => console.log("user disconnect", socket.id));
+
+  socket.on("error", (error) => {
+    console.error(error);
+  });
+});
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/menu", getMenu);
+app.post("/api/order", order);
 
 const port = process.env.PORT;
-app.listen(port);
+server.listen(port);

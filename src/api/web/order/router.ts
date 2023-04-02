d42d9e89c30c2d7a39177f76520edfe82403f839
hwan0hae <eoghkszn@gmail.com>
2023-04-02 13:52:34 +0900

    [feat] order - check, cancel 추가

--- src/api/web/order/router.ts
+++ src/api/web/order/router.ts
@@ -40,7 +40,6 @@ OrderRouter.get(
         );
         return { ...order, orderDetail: object };
       });
-
       return res.status(200).json(data);
     } catch (error) {
       console.error('/api/v1/web/order/request >> ', error);
@@ -50,4 +49,39 @@ OrderRouter.get(
   }
 );
 
+OrderRouter.post('/check', authChecker, async (req: Request, res: Response) => {
+  const user = req.currentUser;
+  const { orderId }: { orderId: number } = req.body;
+  try {
+    client.query(
+      `UPDATE "order" SET order_status=2, modified_at=now() WHERE order_id=${orderId} AND company_id=${user?.company_id}`
+    );
+
+    return res.status(200).json({ message: '주문이 확인 되었습니다.' });
+  } catch (error) {
+    console.error('/api/v1/web/order/check >> ', error);
+
+    return res.status(400).json({ error, message: '뭔가 에러가 떳씁니당.' });
+  }
+});
+
+OrderRouter.post(
+  '/cancel',
+  authChecker,
+  async (req: Request, res: Response) => {
+    const user = req.currentUser;
+    const { orderId }: { orderId: number } = req.body;
+    try {
+      client.query(
+        `UPDATE "order" SET order_status=0, modified_at=now() WHERE order_id=${orderId} AND company_id=${user?.company_id}`
+      );
+
+      return res.status(200).json({ message: '주문이 취소 되었습니다.' });
+    } catch (error) {
+      console.error('/api/v1/web/order/cancel >> ', error);
+
+      return res.status(400).json({ error, message: '뭔가 에러가 떳씁니당.' });
+    }
+  }
+);
 export default OrderRouter;
